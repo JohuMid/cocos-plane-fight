cfd81b2c660cdaaf6be899c2da67e1a90361d5b9
JohuMid <JohuMid@outlook.com>
2025-06-09 21:54:50 +0800

    第一种子弹的发射功能实现

--- assets/Scripts/Player.ts
+++ assets/Scripts/Player.ts
@@ -1,18 +1,32 @@
-import { _decorator, Component, EventTouch, Input, input, Node, Vec3 } from 'cc';
+import { _decorator, Component, EventTouch, Input, input, instantiate, Node, Prefab, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
 export class Player extends Component {
+    @property
+    shootRate: number = 0.5; // 射击频率，单位：秒
+    @property({ type: Node })
+    buttleParent: Node = null; // 子弹父节点
+    
+    @property({ type: Prefab })
+    buttle1Prefab: Prefab = null; // 子弹预设
+
+    @property({ type: Node })
+    buttle1Position:Node = null;
+
+
+    shootTime: number = 0; // 上次射击时间
+
 
     protected onLoad(): void {
         input.on(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
     }
 
     protected onDestroy(): void {
-        input.off(Input.EventType.TOUCH_MOVE, this.onTouchMove, this); 
+        input.off(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
     }
 
-    onTouchMove(event:EventTouch){
+    onTouchMove(event: EventTouch) {
         // 获取触摸的位置
         let delta = event.getDelta();
         const p = this.node.position;
@@ -20,7 +34,7 @@ export class Player extends Component {
         const targetPosition = new Vec3(p.x + delta.x, p.y + delta.y, p.z);
 
         this.node.setPosition(p.x + delta.x, p.y + delta.y, p.z);
-        
+
         // 限制飞机的移动范围
         if (targetPosition.x < -230) {
             this.node.setPosition(-230, p.y, p.z);
@@ -29,7 +43,7 @@ export class Player extends Component {
             this.node.setPosition(230, p.y, p.z);
         }
         if (targetPosition.y < -360) {
-            this.node.setPosition(p.x, -380, p.z);    
+            this.node.setPosition(p.x, -380, p.z);
         }
 
         if (targetPosition.y > 360) {
@@ -37,6 +51,16 @@ export class Player extends Component {
         }
 
     }
+    protected update(deltaTime: number): void {
+        this.shootTime += deltaTime;
+        if (this.shootTime >= this.shootRate) {
+            this.shootTime = 0; // 重置射击时间
+            const buttle1 = instantiate(this.buttle1Prefab); // 实例化子弹
+            this.buttleParent.addChild(buttle1); // 添加到父节点下
+            buttle1.setWorldPosition(this.buttle1Position.worldPosition)
+        }
+    }
+    
 }
 
 
