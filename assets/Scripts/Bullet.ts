607da62815510379cdae9239b43a4fdeed560bfe
JohuMid <JohuMid@outlook.com>
2025-06-09 21:40:13 +0800

    制作子弹预制体发射子弹

---
+++ assets/Scripts/Bullet.ts
@@ -0,0 +1,18 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Bullet')
+export class Bullet extends Component {
+    @property
+    speed: number = 300;
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        const position = this.node.position;
+        this.node.setPosition(position.x, position.y + this.speed * deltaTime, position.z);
+    }
+}
+
+
