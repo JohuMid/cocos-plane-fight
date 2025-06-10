a0a988bc9e9b79383d8c28c2e4c12491e52ec23d
JohuMid <johumid@outlook.com>
2025-06-10 17:51:51 +0800

    飞机击落和飞机销毁

--- assets/Scripts/Enemy.ts
+++ assets/Scripts/Enemy.ts
@@ -1,4 +1,4 @@
-import { _decorator, Animation, animation, Component, Node } from 'cc';
+import { _decorator, Animation, animation, Collider2D, Component, Contact2DType, Node, PhysicsSystem2D } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Enery')
@@ -6,16 +6,58 @@ export class Enery extends Component {
     @property
     speed: number = 300;
 
-    @property({type:Animation})
-    anim:Animation = null;
+    @property({ type: Animation })
+    anim: Animation = null;
+
+    @property
+    hp: number = 1;
+
+    collider: Collider2D = null;
 
     start() {
         // this.anim.play();
+        this.collider = this.getComponent(Collider2D);
+        if (this.collider) {
+            this.collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+        }
+    }
+
+    onBeginContact() {
+        if (this.hp > 0) {
+            this.hp -= 1;
+            if (this.anim) {
+                this.anim.play();
+                if (this.collider) {
+                    this.collider.enabled = false; // 禁用碰撞器
+                }
+            } else {
+                console.error('Animation component not found on enemy node');
+            }
+        }
+        if (this.hp <= 0) {
+            // 销毁敌人节点
+            const that = this
+            this.scheduleOnce(() => {
+                that.node.destroy(); // 销毁敌人节点 
+            }, 1)
+        }
     }
 
     update(deltaTime: number) {
-        const p = this.node.position;
-        this.node.setPosition(p.x, p.y - this.speed * deltaTime, p.z);
+        if (this.hp>0) {
+            const p = this.node.position;
+            this.node.setPosition(p.x, p.y - this.speed * deltaTime, p.z);
+        }
+        if (this.node.position.y < -580) {
+            this.node.destroy(); // 销毁敌人节点    
+        }
+        
+    }
+
+    protected onDestroy(): void {
+        if (this.collider) {
+            this.collider.off(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+        }
     }
 }
 
